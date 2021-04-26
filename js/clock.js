c124e4a6e4f138d92d10e205a451ab5974006c11
MaxShylo <maxshylenko@gmail.com>
2021-04-27 00:36:41 +0200

    js+new blocks

--- js/clock.js
+++
@@ -1,8 +0,0 @@
-function updateClock() {
-    const clock = document.querySelector('.clock');
-    clock.innerText = new Date().toLocaleTimeString();
-    // clock.style.backgroundColor = 'red';
-    clock.classList.add('highlight');
-}
-
-setInterval(updateClock, 1000);
